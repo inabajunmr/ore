fd2507f407a9eff5ed48dc26cea1f6d19401c124
inabajunmr <inabajun.for.regi@gmail.com>
2019-12-21 10:01:09 +0900

    wip

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,12 +1,11 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	if (JSON.parse(request.command) == "ore") {
-		ore(request.imageUrl);
+	var requestJson = JSON.parse(request)
+	if (requestJson.command == "ore") {
+		ore(requestJson.imageUrl);
 	}
-	console.log(request)
 });
 
 function ore(imageUrl) {
-	console.log("ore")
 	imgs = document.querySelectorAll("img")
 	imgs.forEach(function(element) {
 		element.setAttribute("src", imageUrl)
